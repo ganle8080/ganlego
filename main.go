7f46ae2e565d43570593ce10c5b3dd60e86aad33
ganle8080 <siburnjuly@gmail.com>
2024-12-22 10:38:19 +0800

    add

--- main.go
+++ main.go
@@ -3,19 +3,8 @@ package main
 import "fmt"
 
 func main() {
-
-	//声明一个名为nums的数组
-	nums := []int{9, 3, 4, 3, 5}
-
-	//给定一个索引值，将数组切分成为左右两边两个数组，不包含当前索引元素
-
-	numsIndex := 2
-
-	leftNums := nums[:numsIndex]
-	rightNums := nums[numsIndex+1:]
-
-	//打印结果
-	fmt.Printf("leftNums: %v\n", leftNums)
-	fmt.Printf("rightNums: %v\n", rightNums)
-
+	// 定义了一个名为yourMoney的变量，数据类型为int
+	var yourMoney int
+	// 打印输出
+	fmt.Printf("yourMoney: %v\n", yourMoney)
 }
